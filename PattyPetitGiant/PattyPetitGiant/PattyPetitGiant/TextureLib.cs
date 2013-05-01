5690e62fc8f66000837cac95538711be3bf51316
Daniel Savage <dansvg@gmail.com>
2013-05-01 14:31:31 -0700

    skeleton of textureLib

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/TextureLib.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/TextureLib.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace PattyPetitGiant
 {
+    /// <summary>
+    /// Loads textures into memory from file. Storage is abstracted via the singleton pattern.
+    /// </summary>
     class TextureLib
     {
+        private string graphicsDirectory = "Content/gfx/";
+
+        private static Dictionary<string, Texture2D> dict = null;
+
+        public TextureLib()
+        {
+            initTextureLib();
+        }
+
+        public static void initTextureLib()
+        {
+            if (dict == null)
+            {
+                dict = new Dictionary<string, Texture2D>();
+            }
+        }
+
+        public static bool loadTexture(string filename)
+        {
+            return false;
+        }
+
+        public static Texture2D getLoadedTexture(string filename)
+        {
+            return null;
+        }
     }
 }
