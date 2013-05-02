bb4b220e5fd1372ceb26035996fe6803b0aed28d
Daniel Savage <dansvg@gmail.com>
2013-05-02 11:22:44 -0700

    added base classes for game screen states

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/ScreenState.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/ScreenState.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace PattyPetitGiant
 {
-    class ScreenState
+    public abstract class ScreenState
     {
+        protected bool pause = false;
+        public bool Pause { get { return pause; } set { pause = value; } }
+
+        public void update(GameTime currentTime)
+        {
+            if (pause)
+            {
+                return;
+            }
+
+            doUpdate(currentTime);
+        }
+
+        protected abstract void doUpdate(GameTime currentTime);
+
+        protected abstract void draw(SpriteBatch sb);
     }
 }
