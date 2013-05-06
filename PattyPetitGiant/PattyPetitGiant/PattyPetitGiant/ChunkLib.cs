c69ab71f7234bc7f0115afb9ac75a2e50ffa9553
Daniel Savage <dansvg@gmail.com>
2013-05-06 11:18:03 -0700

    Added template content for chunk loading

---
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/ChunkLib.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PattyPetitGiant
+{
+    /// <summary>
+    /// Maintains a library of game chunks for dungeon generation.
+    /// </summary>
+    class ChunkLib
+    {
+        private static string chunkDirectory = "Content/chunk";
+        private static string chunkManifest = "Content/chunk/manifest.txt";
+
+        private static Dictionary<string, ChunkManager.Chunk> dict = null;
+
+        public ChunkLib()
+        {
+            initLib();
+        }
+
+        public static void initLib()
+        {
+            if (dict == null)
+            {
+                dict = new Dictionary<string, ChunkManager.Chunk>();
+            }
+        }
+
+        private static void loadManifest()
+        {
+            //
+        }
+    }
+}
