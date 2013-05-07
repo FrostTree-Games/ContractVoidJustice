c853b21c56ee4297927ea8f996760cd374bb6f2e
Daniel Savage <dansvg@gmail.com>
2013-05-07 09:51:24 -0700

    Added clone of Spine framework to project.

---
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/AnimationLib.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spine;
+
+namespace PattyPetitGiant
+{
+    class AnimationLib
+    {
+        private static bool initalized = false;
+    }
+}
