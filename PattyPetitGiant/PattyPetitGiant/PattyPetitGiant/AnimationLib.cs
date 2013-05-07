87e8f6ed6aa30d9c2fe0674c0dc8eb446aec2055
Daniel Savage <dansvg@gmail.com>
2013-05-07 10:32:58 -0700

    Some AnimationLib prep work

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/AnimationLib.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/AnimationLib.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Spine;
 
 namespace PattyPetitGiant
 {
-    class AnimationLib
+    public class AnimationLib
     {
-        private static bool initalized = false;
+        private static XnaTextureLoader textureLoader = null;
+        private static SkeletonRenderer skeletonRenderer = null;
+
+        private static SpriteBatch spriteBatch = null;
+
+        private struct SpineAnimation
+        {
+            public string fileName;
+            public Spine.Atlas atlas;
+            public Skeleton skeleton;
+            public Animation animation;
+            public float animationTime;
+        }
+
+        public AnimationLib(GraphicsDevice gd, SpriteBatch spriteBatch)
+        {
+            AnimationLib.spriteBatch = spriteBatch;
+
+            AnimationLib.textureLoader = new XnaTextureLoader(gd);
+            AnimationLib.skeletonRenderer = new SkeletonRenderer(gd);
+        }
     }
 }
