13b97c44a9647112cb516d5c82bbaeebf2da0777
Eric Chan <ettchan618@gmail.com>
2013-05-08 12:21:23 -0700

    Player health, ammo, coin can be accessed by anyone and can be displayed. Added coin class

---
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PattyPetitGiant
+{
+    class Coin : Entity
+    {
+        public Coin()
+        {
+        }
+        public Coin(float initial_x, float initial_y, LevelState parentWorld)
+        {
+            position = new Vector2(initial_x, initial_y);
+            this.parentWorld = parentWorld;
+        }
+    }
+}
