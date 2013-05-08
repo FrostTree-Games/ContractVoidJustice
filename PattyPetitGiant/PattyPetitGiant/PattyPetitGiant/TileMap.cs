4d592d3ac24b4084767734067cfd2471c24ebd63
Daniel Savage <dansvg@gmail.com>
2013-05-08 11:03:35 -0700

    Removed test TileMap code.

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/TileMap.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/TileMap.cs
@@ -86,31 +86,6 @@ namespace PattyPetitGiant
         private Texture2D tileSkin = null;
         public Texture2D TileSkin { get { return tileSkin; } set { tileSkin = value; } }
 
-        /*
-        public TileMap(TileDimensions size, Vector2 tileSize)
-        {
-            this.size = size;
-            this.tileSize = tileSize;
-
-            map = new TileType[size.x, size.y];
-            blobTestWalls();
-
-            startPosition = new Vector2(0, 0);
-        }
-
-        public TileMap(TileDimensions size, Vector2 tileSize, Texture2D tileMap)
-        {
-            this.size = size;
-            this.tileSize = tileSize;
-
-            map = new TileType[size.x, size.y];
-            blobTestWalls();
-
-            startPosition = new Vector2(0, 0);
-
-            this.tileSkin = tileMap;
-        } */
-
         public TileMap(DungeonGenerator.DungeonRoom[,] room, Vector2 tileSize)
         {
             this.size = new TileDimensions(room.GetLength(0) * GlobalGameConstants.TilesPerRoomWide, room.GetLength(1)  * GlobalGameConstants.TilesPerRoomHigh);
@@ -175,6 +150,7 @@ namespace PattyPetitGiant
                     }
                     else
                     {
+                        //fill a room with empty tiles if it is empty
                         for (int p = 0; p < GlobalGameConstants.TilesPerRoomWide; p++)
                         {
                             for (int q = 0; q < GlobalGameConstants.TilesPerRoomHigh; q++)
@@ -183,66 +159,10 @@ namespace PattyPetitGiant
                             }
                         }
                     }
-
-#if PROTOTYPE_ROOMS
-                    for (int p = 0; p < GlobalGameConstants.TilesPerRoomWide; p++)
-                    {
-                        for (int q = 0; q < GlobalGameConstants.TilesPerRoomHigh; q++)
-                        {
-                            if (!room[i, j].north && !room[i, j].south && !room[i, j].west && !room[i, j].east)
-                            {
-                                map[(i * GlobalGameConstants.TilesPerRoomWide) + p, (j * GlobalGameConstants.TilesPerRoomHigh) + q] = TileType.TestWall;
-                                continue;
-                            }
-
-                            if ((q == 0 || q == 1) && ((p == GlobalGameConstants.TilesPerRoomWide / 2) || (p == GlobalGameConstants.TilesPerRoomWide / 2 - 1)) && room[i, j].north)
-                            {
-                                continue;
-                            }
-                            if ((q == GlobalGameConstants.TilesPerRoomHigh - 1 || q == GlobalGameConstants.TilesPerRoomHigh - 2) && ((p == GlobalGameConstants.TilesPerRoomWide / 2) || (p == GlobalGameConstants.TilesPerRoomWide / 2 - 1)) && room[i, j].south)
-                            {
-                                continue;
-                            }
-
-                            if ((p == 0 || p == 1) && ((q == GlobalGameConstants.TilesPerRoomHigh / 2) || (q == GlobalGameConstants.TilesPerRoomHigh / 2 - 1)) && room[i, j].west)
-                            {
-                                continue;
-                            }
-                            if ((p == GlobalGameConstants.TilesPerRoomWide - 1 || p == GlobalGameConstants.TilesPerRoomWide - 2) && ((q == GlobalGameConstants.TilesPerRoomWide / 2) || (q == GlobalGameConstants.TilesPerRoomWide / 2 - 1)) && room[i, j].east)
-                            {
-                                continue;
-                            }
-
-                            if (p == 0 || p == 1 || p == GlobalGameConstants.TilesPerRoomWide - 1 || p == GlobalGameConstants.TilesPerRoomWide - 2)
-                            {
-                                map[(i * GlobalGameConstants.TilesPerRoomWide) + p, (j * GlobalGameConstants.TilesPerRoomHigh) + q] = TileType.TestWall;
-                            }
-                            if (q == 0 || q == 1 || q == GlobalGameConstants.TilesPerRoomHigh - 1 || q == GlobalGameConstants.TilesPerRoomHigh - 2)
-                            {
-                                map[(i * GlobalGameConstants.TilesPerRoomWide) + p, (j * GlobalGameConstants.TilesPerRoomHigh) + q] = TileType.TestWall;
-                            }
-                        }
-                    }
-#endif
                 }
             }
 
             convertTestWallsToWalls();
-        }
-
-        public TileMap(TileDimensions size, Vector2 tileSize, TileType initType)
-        {
-            this.size = size;
-            this.tileSize = tileSize;
-
-            map = new TileType[size.x, size.y];
-            for (int i = 0; i < size.x; i++)
-            {
-                for (int j = 0; j < size.y; j++)
-                {
-                    map[i, j] = initType;
-                }
-            }
         }
 
         /// <summary>
