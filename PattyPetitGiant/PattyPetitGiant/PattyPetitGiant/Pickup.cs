f3ad4cea8cc34c7c6efef632efb212e866e07466
Eric Chan <ettchan618@gmail.com>
2013-05-09 10:29:53 -0700

    got rid of some classes

---
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/Pickup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PattyPetitGiant
+{
+    class Pickup : Entity
+    {
+        GlobalGameConstants.itemType item_type = GlobalGameConstants.itemType.Bomb;
+        public Pickup()
+        {
+            
+        }
+        public void update(Player parent, GameTime currentTime, LevelState parentWorld)
+        {
+
+        }
+        public void daemonupdate(GameTime currentTime, LevelState parentWorld)
+        {
+        }
+        public void draw(SpriteBatch sb)
+        {
+        }
+    }
+}
