08ebfeb1b15fe84e7c079c84ef95f68278f6f79d
Eric Chan <ettchan618@gmail.com>
2013-05-21 11:25:24 -0700

    added classes

---
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/IdleLook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace PattyPetitGiant
+{
+    class IdleLook : EnemyComponents
+    {
+        public void update(Enemy parent, GameTime currentTime, LevelState parentWorld)
+        {
+            return;
+        }
+        public void update(Enemy parent, Entity player, GameTime currentTime, LevelState parentWorld)
+        {
+            return;
+        }
+
+    }
+}
