d57918eef23a484d199c7c31c6e71103eb1a8ac3
Daniel Savage <dansvg@gmail.com>
2013-05-29 12:19:07 -0700

    Added lock/key classes

---
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/KeySystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PattyPetitGiant
+{
+    class KeyDoor : Entity
+    {
+        public override void knockBack(Vector2 direction, float magnitude, int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void update(GameTime currentTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void draw(SpriteBatch sb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Key : Entity
+    {
+        public override void knockBack(Vector2 direction, float magnitude, int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void update(GameTime currentTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void draw(SpriteBatch sb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
