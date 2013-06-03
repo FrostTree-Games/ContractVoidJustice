f7bc4353d19778c85fb086af8cfcf0d948c10863
Daniel Savage <dansvg@gmail.com>
2013-06-03 11:32:55 -0700

    Added base MolotovEnemy and updated tilemap

---
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/MolotovEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PattyPetitGiant
+{
+    class MolotovEnemy : Enemy
+    {
+        public override void update(GameTime currentTime)
+        {
+            base.update(currentTime);
+        }
+
+        public override void draw(SpriteBatch sb)
+        {
+            base.draw(sb);
+        }
+
+        public override void knockBack(Vector2 direction, float magnitude, int damage)
+        {
+            base.knockBack(direction, magnitude, damage);
+        }
+
+        public override void spinerender(Spine.SkeletonRenderer renderer)
+        {
+            base.spinerender(renderer);
+        }
+    }
+}
