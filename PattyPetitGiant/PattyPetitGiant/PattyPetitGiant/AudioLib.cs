93c6b26db97f85827668ee4340453a7bc766f8cb
Daniel Savage <dansvg@gmail.com>
2013-06-10 16:33:23 -0700

    Added SFX loading and playback.

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/AudioLib.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/AudioLib.cs
@@ -1,12 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
 
 namespace PattyPetitGiant
 {
-    class AudioLib
+    public class AudioLib
     {
+        private static string sfxDirectory = "Content/sfx/";
+        private static string sfxManifestFile = "Content/sfx/manifest.txt";
 
+        private static string sfxFileExtension = ".wav";
+
+        private static Dictionary<string, SoundEffect> sfxLib = null;
+        private static bool sfxManifestLoaded = false;
+
+        public AudioLib()
+        {
+            if (sfxLib == null)
+            {
+                sfxLib = new Dictionary<string, SoundEffect>();
+            }
+
+            if (!sfxManifestLoaded)
+            {
+                loadManifest();
+            }
+        }
+
+        private static bool loadSFX(string url)
+        {
+            SoundEffect s = null;
+            FileStream fs = null;
+
+            try
+            {
+                fs = new FileStream(sfxDirectory + url + sfxFileExtension, FileMode.Open, FileAccess.Read);
+                s = SoundEffect.FromStream(fs);
+            }
+            catch (FileNotFoundException)
+            {
+                return false;
+            }
+
+            sfxLib.Add(url, s);
+
+            return true;
+        }
+
+        private static void loadManifest()
+        {
+            if (sfxManifestLoaded || sfxLib == null)
+            {
+                return;
+            }
+
+#if WINDOWS
+            string[] frameAnims = File.ReadAllLines(sfxManifestFile);
+
+            foreach (string line in frameAnims)
+            {
+                loadSFX(line);
+            }
+
+#elif XBOX
+            String xboxLine;
+            int counter = 0;
+
+            StreamReader file = new StreamReader(sfxManifestFile);
+
+            while ((xboxLine = file.ReadLine()) != null)
+            {
+                loadSFX(xboxLine);
+
+                counter++;
+            }
+
+            file.Close();
+
+#endif
+
+            sfxManifestLoaded = true;
+        }
+
+        /// <summary>
+        /// Acquire a sound effect from the library.
+        /// </summary>
+        /// <param name="sfxName">The name of the sound effect as specified in the manifest file.</param>
+        /// <returns></returns>
+        public static void playSoundEffect(string sfxName)
+        {
+            sfxLib[sfxName].Play();
+        }
     }
 }
