df56e59808fdc46ba992d5c2f938437bb5360b59
Daniel Savage <dansvg@gmail.com>
2013-06-11 15:00:36 -0700

    RiotShield Item skeleton

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/RiotShield.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/RiotShield.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace PattyPetitGiant
 {
-    class RiotShield
+    class RiotShield : Item
     {
+        public RiotShield()
+        {
+            //
+        }
+
+        public void update(Player parent, GameTime currentTime, LevelState parentWorld)
+        {
+            //
+        }
+
+        public void daemonupdate(Player parent, GameTime currentTime, LevelState parentWorld)
+        {
+            //
+        }
+
+        public void draw(SpriteBatch sb)
+        {
+            //
+        }
+
+        public GlobalGameConstants.itemType ItemType()
+        {
+            return GlobalGameConstants.itemType.RiotShield;
+        }
+
+        public string getEnumType()
+        {
+            return "RiotShield";
+        }
     }
 }
