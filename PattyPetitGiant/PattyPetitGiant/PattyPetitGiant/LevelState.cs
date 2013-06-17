f7edcacf7612fe3abee5520329ab5421c7128b86
Daniel Savage <dansvg@gmail.com>
2013-06-17 10:38:22 -0700

    Cleaned levelstate spawning

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/LevelState.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/LevelState.cs
@@ -175,12 +175,6 @@ namespace PattyPetitGiant
                     else if (rooms[i, j].attributes.Contains("start"))
                     {
                         entityList.Add(new Player(this, (currentRoomX + 8) * GlobalGameConstants.TileSize.X, (currentRoomY + 8) * GlobalGameConstants.TileSize.Y));
-
-                        for (int q = 0; q < 20; q++)
-                        {
-                            entityList.Add(new SnakeWorm(this, new Vector2((currentRoomX) * GlobalGameConstants.TileSize.X + 100, (currentRoomY + 10) * GlobalGameConstants.TileSize.Y)));
-                        }
-                        //entityList.Add(new GuardSquadLeader(this, new Vector2((currentRoomX ) * GlobalGameConstants.TileSize.X, (currentRoomY + 10) * GlobalGameConstants.TileSize.Y)));
                     }
                     else if (rooms[i, j].attributes.Contains("end"))
                     {
