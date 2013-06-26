0fbb479e5ff5058029ebad3473a6a9ec3dbffcd2
Eric Chan <ettchan618@gmail.com>
2013-06-26 13:37:15 -0700

    started work on acid spitter

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/MutantAcidSpitter.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/MutantAcidSpitter.cs
@@ -2,10 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Spine;
 
 namespace PattyPetitGiant
 {
-    class MutantAcidSpitter
+    class MutantAcidSpitter : Enemy
     {
+        private enum SpitterState
+        {
+            Search,
+            Fire,
+            Reset
+        }
+
+        private bool death;
+
+        public MutantAcidSpitter(LevelState parentWorld, float initial_x, float initial_y)
+        {
+            position = new Vector2(initial_x, initial_y);
+            dimensions = new Vector2(48.0f, 48.0f);
+
+            enemy_damage = 4;
+            enemy_life = 20;
+
+            death = false;
+        }
+
+        public override void update(GameTime currentTime)
+        {
+            
+        }
+
+        public override void draw(SpriteBatch sb)
+        {
+            base.draw(sb);
+        }
+
+        public override void knockBack(Vector2 direction, float magnitude, int damage, Entity attacker)
+        {
+            //base.knockBack(direction, magnitude, damage, attacker);
+        }
+
+        public override void spinerender(SkeletonRenderer renderer)
+        {
+            //base.spinerender(renderer);
+        }
     }
 }
