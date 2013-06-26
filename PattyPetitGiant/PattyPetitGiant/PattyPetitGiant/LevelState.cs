f561e916c1b31f97ff40be3b72bedde21d8fecc8
Daniel Savage <dansvg@gmail.com>
2013-06-26 13:42:14 -0700

    Added GuardMech prototype to spawner

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/LevelState.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/LevelState.cs
@@ -160,7 +160,7 @@ namespace PattyPetitGiant
                     }
                     else if (randomSpawnValue < 0.5)
                     {
-                        //entityList.Add(new ChargerMutantEnemy(this, spawnPos));
+                        entityList.Add(new GuardMech(this, spawnPos.X, spawnPos.Y));
                     }
                     else
                     {
@@ -253,8 +253,6 @@ namespace PattyPetitGiant
                     else if (rooms[i, j].attributes.Contains("start"))
                     {
                         entityList.Add(new Player(this, (currentRoomX + 8) * GlobalGameConstants.TileSize.X, (currentRoomY + 8) * GlobalGameConstants.TileSize.Y));
-                        entityList.Add(new GuardMech(this, (currentRoomX + 8) * GlobalGameConstants.TileSize.X, (currentRoomY) * GlobalGameConstants.TileSize.Y));
-                        //entityList.Add(new BroodLord(this, new Vector2((currentRoomX + 8) * GlobalGameConstants.TileSize.X + 200, (currentRoomY) * GlobalGameConstants.TileSize.Y + 300)));
                     }
                     else if (rooms[i, j].attributes.Contains("pickup"))
                     {
