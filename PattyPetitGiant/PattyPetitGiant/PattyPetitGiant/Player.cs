5ae792cf997eab2a252fb4d431efb6fef36dfbb5
Daniel Savage <dansvg@gmail.com>
2013-07-04 12:36:58 -0700

    Quick item switch

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/Player.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/Player.cs
@@ -73,7 +73,7 @@ namespace PattyPetitGiant
             velocity = Vector2.Zero;
 
             player_item_1 = new Sword();
-            player_item_2 = new ShotGun();
+            player_item_2 = new DungeonMap();
             GameCampaign.Player_Item_1 = player_item_1.getEnumType();
             GameCampaign.Player_Item_2 = player_item_2.getEnumType();
 
