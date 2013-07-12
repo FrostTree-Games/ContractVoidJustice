34036856508a2c5b06400d50d599ce230faacf6a
Eric Chan <ettchan618@gmail.com>
2013-07-11 17:01:50 -0700

    title screen is able to transition with music being produced

---
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/BackGroundAudio.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace PattyPetitGiant
+{
+    class BackGroundAudio
+    {
+        public bool finished_song = false;
+
+        private static Song song = null;
+        public static SongCollection songs = null;
+
+        public static Dictionary<string, Song> bgm_list = new Dictionary<string,Song>();
+
+        private static ContentManager content_manager = null;
+
+        public BackGroundAudio(ContentManager manager)
+        {
+            MediaPlayer.IsRepeating = true;
+            content_manager = manager;
+        }
+
+        public void addSong(string new_song)
+        {
+            //sampleMediaLibrary.Songs;
+            bgm_list.Add(new_song, content_manager.Load<Song>("bgm/"+new_song));
+        }
+
+        public static void playSong(string song_name, bool repeat)
+        {
+            if (!bgm_list.TryGetValue(song_name, out song))
+            {
+                throw new ArgumentException("No song name " + song_name + " exists");
+            }
+
+            MediaPlayer.Play(song);
+            MediaPlayer.IsRepeating = repeat;
+        }
+    }
+}
