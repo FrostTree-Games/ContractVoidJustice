89e4e7fd71d6a4e77033aa11e5f8a5adcc0ab949
Daniel Savage <dansvg@gmail.com>
2013-08-06 10:51:39 -0700

    High scores has slide-in and rankings

--- PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/HighScoresState.cs
+++ PattyPetitGiant/PattyPetitGiant/PattyPetitGiant/HighScoresState.cs
@@ -101,9 +101,12 @@ namespace PattyPetitGiant
 
         private static List<HighScoreValue> highScores = null;
 
+        private int newlyAddedScoreIndex = -2;
+
         private HighScoreStateScreenAnimationState state;
         private float stateTimer;
-        private const float preWaitDuration = 300f;
+        private const float preWaitDuration = 600f;
+        private const float slideInDuration = 300f;
 
         public static void InitalizeHighScores()
         {
@@ -118,6 +121,10 @@ namespace PattyPetitGiant
                 highScores.Add(new HighScoreValue("Ivan", 1000, 345, 0, 1));
                 highScores.Add(new HighScoreValue("Alyosha", 1000, 345, 0, 1));
                 highScores.Add(new HighScoreValue("Dmitri", 1500, 43567, 4, 2));
+                highScores.Add(new HighScoreValue("Gunther", 2233, 2332, 2, 1));
+                highScores.Add(new HighScoreValue("lolololol", 2020, 333, 1, 0));
+                highScores.Add(new HighScoreValue("quinten", 20202, 9981, 1, 1));
+                highScores.Add(new HighScoreValue("Zippy", 200, 000, 5, 1));
             }
         }
 
@@ -126,9 +133,36 @@ namespace PattyPetitGiant
             InitalizeHighScores();
 
             HighScoreValue newScore = new HighScoreValue(GameCampaign.PlayerName, GameCampaign.Player_Coin_Amount, GameCampaign.ElapsedCampaignTime, GameCampaign.PlayerLevelProgress, GameCampaign.PlayerFloorHeight);
+
             highScores.Add(newScore);
+
+            // if there are 11 high scores now, remove the lowest score
+            if (highScores.Count > 10)
+            {
+                HighScoreValue lowestScore = highScores[0];
+                CompareHighScores comparer = new CompareHighScores();
+
+                for (int i = 1; i < highScores.Count; i++)
+                {
+                    if (comparer.Compare(highScores[i], lowestScore) < 0)
+                    {
+                        lowestScore = highScores[i];
+                    }
+                }
+
+                highScores.Remove(lowestScore);
+            }
+
             highScores.Sort(new CompareHighScores());
             highScores.Reverse();
+
+            for (int i = 0; i < highScores.Count; i++)
+            {
+                if (highScores[i] == newScore)
+                {
+                    newlyAddedScoreIndex = i;
+                }
+            }
 
             state = HighScoreStateScreenAnimationState.Start;
             stateTimer = 0;
@@ -140,6 +174,12 @@ namespace PattyPetitGiant
 
             if (state == HighScoreStateScreenAnimationState.Start)
             {
+                if (newlyAddedScoreIndex == -2)
+                {
+                    stateTimer = -300;
+                    state = HighScoreStateScreenAnimationState.IdleWait;
+                }
+
                 if (stateTimer > preWaitDuration)
                 {
                     stateTimer = 0;
@@ -148,7 +188,11 @@ namespace PattyPetitGiant
             }
             else if (state == HighScoreStateScreenAnimationState.SlideNewHighScoreIn)
             {
-                //
+                if (stateTimer > slideInDuration)
+                {
+                    stateTimer = 0;
+                    state = HighScoreStateScreenAnimationState.IdleWait;
+                }
             }
             else
             {
@@ -164,9 +208,38 @@ namespace PattyPetitGiant
 
             for (int i = 0; i < highScores.Count; i++)
             {
-                Vector2 drawListPosition = new Vector2(100) + new Vector2(0, i * 28);
+                if (state == HighScoreStateScreenAnimationState.Start)
+                {
+                    if (i == newlyAddedScoreIndex) { continue; }
 
-                sb.DrawString(Game1.font, highScores[i].playerName + " died on " + highScores[i].levelDiedAt + " with " + highScores[i].coinCollected + " after " + highScores[i].secondsElapsed, drawListPosition, Color.White);
+                    Vector2 drawListPosition = new Vector2(100) + new Vector2(0, i * 28);
+                    if (i > newlyAddedScoreIndex) { drawListPosition.Y -= 28; }
+
+                    sb.DrawString(Game1.font, highScores[i].playerName + " died on " + highScores[i].levelDiedAt + " with " + highScores[i].coinCollected + " after " + highScores[i].secondsElapsed, drawListPosition, Color.White);
+                }
+                else if (state == HighScoreStateScreenAnimationState.SlideNewHighScoreIn)
+                {
+                    if (i == newlyAddedScoreIndex)
+                    {
+                        Vector2 drawListPosition = new Vector2(100) + new Vector2(-200, i * 28);
+                        drawListPosition.X += 200 * (stateTimer / slideInDuration);
+
+                        sb.DrawString(Game1.font, highScores[i].playerName + " died on " + highScores[i].levelDiedAt + " with " + highScores[i].coinCollected + " after " + highScores[i].secondsElapsed, drawListPosition, Color.White);
+                    }
+                    else
+                    {
+                        Vector2 drawListPosition = new Vector2(100) + new Vector2(0, i * 28);
+                        if (i > newlyAddedScoreIndex) { drawListPosition.Y -= 28 * (1 - (stateTimer / slideInDuration)); }
+
+                        sb.DrawString(Game1.font, highScores[i].playerName + " died on " + highScores[i].levelDiedAt + " with " + highScores[i].coinCollected + " after " + highScores[i].secondsElapsed, drawListPosition, Color.White);
+                    }
+                }
+                else
+                {
+                    Vector2 drawListPosition = new Vector2(100) + new Vector2(0, i * 28);
+
+                    sb.DrawString(Game1.font, highScores[i].playerName + " died on " + highScores[i].levelDiedAt + " with " + highScores[i].coinCollected + " after " + highScores[i].secondsElapsed, drawListPosition, Color.White);
+                }
             }
 
             sb.End();
